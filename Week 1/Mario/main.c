2405d12226bb43885962527c3d4781ebcab3590f
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-08-19 11:07:23 +0300

    2.0

--- Week 1/Mario/main.c
+++ Week 1/Mario/main.c
@@ -1,9 +1,47 @@
 #include<stdio.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+# define MAX_INPUT 100
+
+int isTrue = 1;
+
+int theLoop(int n) {
+    int i, j, s = 0, pattern = 0;
+
+    for (i = 0; i < n; i++) {
+        s = n - (i + 1);
+        for (j = 0; j < s; j++) {
+            printf(" ");
+        }
+
+        pattern = i + 1;
+
+        for (j = 0; j < pattern; j++) {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+    isTrue = 0;
+}
+
 
 int main() {
-    int n, i,j,s=0, pattern=0;
-    printf("Height:");
-    scanf("%d", n);
+    int n;
 
-    printf(n);
+    while (isTrue) {
+
+        printf("Height: ");
+        scanf("%d", &n);
+        printf("\n");
+
+        if (1 <= n && n <= 8) {
+            theLoop(n);
+        }
+    }
 }
+
+
